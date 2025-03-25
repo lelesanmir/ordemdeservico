60f9807a89c17d08294709db16ddea223e3d85f5
lelesanmir <leonardodossantosmiranda@gmail.com>
2025-03-25 20:46:28 -0300

    Iniciando projeto no repositorio.

---
+++ src/main/java/com/leosanmir/ordemdeservico/OrdemdeservicoApplication.java
@@ -0,0 +1,13 @@
+package com.leosanmir.ordemdeservico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdemdeservicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdemdeservicoApplication.class, args);
+	}
+
+}
